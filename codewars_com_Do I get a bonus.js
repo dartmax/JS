c456218a20def0cf3a28bab4c20ac12aab8296c2
Max.Seryodkin <promostend@gmail.com>
2018-03-21 02:55:40 +0200

    Create codewars_com_Do I get a bonus.js
    8 kyu

---
+++ codewars_com_Do I get a bonus.js
@@ -0,0 +1,16 @@
+function bonusTime(salary, bonus) {
+if(bonus == true){ 
+    return salary = ('£'+salary * 10);
+}else{ if(bonus == false){
+    return salary = ('£'+salary * 1);
+    }
+}
+ return bonusTime(10000, true);
+ return bonusTime(25000, true);
+ return bonusTime(10000, false);
+ return bonusTime(60000, false);
+ return bonusTime(2, true);
+ return bonusTime(78, false);
+ return bonusTime(67890, true);
+ 
+}
